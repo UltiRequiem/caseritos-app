a8913d742da3f280a660b5bfc5c32b642017b7da
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-05-17 22:15:05 -0500

    chore: 7k

--- backend/did-service/src/configs/melon.config.ts
+++ backend/did-service/src/configs/melon.config.ts
@@ -3,7 +3,15 @@ import { Wallet } from 'ethers';
 import { createDidNearFromPrivateKey } from '@kaytrust/did-near';
 
 export default () => {
-  const { MELON_PRIVATE_KEY, MELON_ETHR_DID, MELON_NEAR_DID, MELON_ISSUER_NAME, NETWORK_CHAIN_ID = 80002 } = process.env;
+  const {
+    MELON_PRIVATE_KEY = Wallet.createRandom().privateKey,
+    MELON_ETHR_DID,
+    MELON_NEAR_DID,
+    MELON_ISSUER_NAME,
+    NETWORK_CHAIN_ID = 80002,
+  } = process.env;
+
+  console.log('MELON_PRIVATE_KEY', MELON_PRIVATE_KEY);
 
   if (!MELON_PRIVATE_KEY)
     throw Error('Required env variable: MELON_PRIVATE_KEY');
@@ -13,11 +21,14 @@ export default () => {
 
   if (!MELON_ETHR_DID) {
     const wallet = new Wallet(MELON_PRIVATE_KEY);
-    const didEthr = createDidEthr(wallet.address, {chainNameOrId: NETWORK_CHAIN_ID});
+    const didEthr = createDidEthr(wallet.address, {
+      chainNameOrId: NETWORK_CHAIN_ID,
+    });
     did = didEthr.did;
   }
   if (!did_near) {
     did_near = createDidNearFromPrivateKey(MELON_PRIVATE_KEY);
+    console.log('did_near', did_near);
   }
 
   return {
